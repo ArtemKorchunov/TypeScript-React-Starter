ccbd36fa7e7916672d889fe92644e71af75abdc9
artemkorchunof <artem.korchunof@gmail.com>
2018-09-21 12:39:07 +0300

    some changes pretty

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -4,18 +4,21 @@ import "./styles.css";
 import { Props } from "../../containers/main";
 
 function Component({ count, addCount }: Props) {
-  return (
-    <div className="hello">
-      <div className="greeting">Hello {count}</div>
-      <div>
+    return (
+        <div className="hello">
+            <div className="greeting">Hello {count}</div>
+            <div>
 
 
 
-        <button onClick={() => addCount(count    + 1 )
-        }>+</button>
-      </div>
-    </div>
-  );
+
+
+
+                <button onClick={() => addCount(count + 1)
+                }>+</button>
+            </div>
+        </div>
+    );
 }
 
 export default Component;
